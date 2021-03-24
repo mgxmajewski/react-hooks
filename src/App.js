16768f74cdc202702e60cf99e83909c530fd48da
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-24 15:19:33 +0100

    Convert App to be stateful component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome!</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            score: 0
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Welcome!</h1>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
