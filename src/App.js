0422b03fe3a99745644a27efe1c81bdb865511df
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-24 19:11:17 +0100

    Add buttons to decrease and reset score

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-    const [ score, setScore ] = useState(0);
+    const [ score, setScore ] = useState(0)
+    const [ message, setMessage ] = useState('Welcome')
     return (
         <div className="App">
             <header className="App-header">
-                <h1>{score}</h1>
-                <button onClick={ () => setScore(score+1)}>
+                <h1>{ message}</h1>
+                <h2>{ score }</h2>
+                <button onClick={() => setScore(prevScore => prevScore + 1)}>
                     Increase score
+                </button>
+                <button onClick={() => setScore(prevScore => prevScore - 1)}>
+                    Decrease score
+                </button>
+                <button onClick={() => setScore(0)}>
+                    Reset score
                 </button>
             </header>
         </div>
